d74af31e4157d9e20819cfc48b0a1a763c660aa6
SannaDias <sannapdias@gmail.com>
2025-12-16 17:21:27 -0300

    [FEAT] Implementando um serviço de worker consumidor e processamento em segundo plano com rabbitmq

--- SistemaAlerta/Seguranca.Worker/Worker.cs
+++ SistemaAlerta/Seguranca.Worker/Worker.cs
@@ -1,22 +1,45 @@
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
 namespace Seguranca.Worker;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
 
-    public Worker(ILogger<Worker> logger)
-    {
-        _logger = logger;
-    }
+    public Worker(ILogger<Worker> logger) => _logger = logger;
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using var connection = await factory.CreateConnectionAsync();
+        using var channel = await connection.CreateChannelAsync();
+
+        // Declaramos a mesma fila que a API usa
+        await channel.QueueDeclareAsync(queue: "fila_login", durable: false, exclusive: false, autoDelete: false);
+
+        _logger.LogInformation(" [*] Worker aguardando mensagens. Para sair, pressione CTRL+C");
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        
+        consumer.ReceivedAsync += async (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var mensagem = Encoding.UTF8.GetString(body);
+
+            _logger.LogInformation(" [x] MENSAGEM RECEBIDA: {0}", mensagem);
+            _logger.LogInformation(" [!] Processando envio de 2FA...");
+            
+            await Task.Delay(2000); // Simulando o tempo de disparo do e-mail
+            
+            _logger.LogInformation(" [V] Alerta enviado com sucesso para o usuário!");
+        };
+
+        await channel.BasicConsumeAsync(queue: "fila_login", autoAck: true, consumer: consumer);
+
         while (!stoppingToken.IsCancellationRequested)
         {
-            if (_logger.IsEnabled(LogLevel.Information))
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            }
             await Task.Delay(1000, stoppingToken);
         }
     }
